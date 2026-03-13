15f05ba586523c54397dc8a9cc402a2c2f1c5fd3
tineqv <pochta13522@gmail.com>
2026-03-13 17:02:38 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Автоматизация для Lenovo G50-30 (Full Auto + User)
+# Автоматизация для Lenovo G50-30 (Hyprland + User + Fixes)
 
 # 1. Форматирование
 mkfs.fat -F 32 /dev/sda1
@@ -7,8 +7,9 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 mount --mkdir /dev/sda1 /mnt/efi
 
-# 2. Установка системы + sudo и софт для Intel
-pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware intel-ucode nano git sudo mesa xf86-video-intel
+# 2. Установка системы + Драйверы Intel + Hyprland
+# Добавил seatd и polkit (нужны для запуска Wayland от пользователя)
+pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware intel-ucode nano git sudo mesa xf86-video-intel hyprland i3-wm kitty waybar networkmanager seatd polkit
 
 # 3. Генерация fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -22,11 +23,15 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "ffeflin-laptop" > /etc/hostname
 
-# Настройка пользователя uraac
-useradd -m -G wheel -s /bin/bash uraac
+# Пользователь uraac и права
+useradd -m -G wheel,video,input -s /bin/bash uraac
 echo "uraac:hh8A7us!" | chpasswd
 echo "root:hh8A7us!" | chpasswd
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
+
+# Включаем нужные сервисы
+systemctl enable NetworkManager
+systemctl enable seatd
 
 # Настройка UKI (Unified Kernel Image)
 mkdir -p /etc/kernel
@@ -38,15 +43,13 @@ sed -i 's|#default_uki="/vmlinuz-linux"|default_uki="/efi/EFI/Linux/arch-zen.efi
 mkdir -p /efi/EFI/Linux
 mkinitcpio -P
 
-# Резервный путь для загрузки (Fallback)
+# Резервный путь загрузки (на случай если UEFI Lenovo сбросит настройки)
 mkdir -p /efi/EFI/BOOT
 cp /efi/EFI/Linux/arch-zen.efi /efi/EFI/BOOT/BOOTX64.EFI
 
-# Загрузчик в NVRAM
-pacman -S --noconfirm efibootmgr hyprland i3-wm kitty waybar NetworkManager
+# Запись в NVRAM
+pacman -S --noconfirm efibootmgr
 efibootmgr --create --disk /dev/sda --part 1 --label "Arch Zen" --loader 'EFI\Linux\arch-zen.efi' --unicode
-
-systemctl enable NetworkManager
 EOF
 
-echo "Готово! Система настроена, пользователь uraac создан."
+echo "Установка завершена! После ребута логинься как uraac и вводи 'Hyprland'."
